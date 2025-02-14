a2cc5f43711f6ffa67693f349bb0ec95f64ba9c1
T0PSH31F <106733950+T0PSH31F@users.noreply.github.com>
2025-02-13 21:40:36 -0800

    Create config.nix

---
+++ modules/flake-parts/config.nix
@@ -0,0 +1,10 @@
+# Global configuration for this repo
+#
+# See ./modules/flake-parts/config-module.nix for schema
+{
+  me = {
+    username = "t0psh31f";
+    fullname = "Erik Wright";
+    email = "wrighterik77@gmail.com";
+  };
+}
